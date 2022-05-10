ef4655282644ee4f8504db3edb37fd6c72845abd
sinuhem23 <sinuhem23@gmail.com>
2022-05-10 14:15:01 -0400

    random colors 0-5 are showing properly

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
-function App() {
-  return <div></div>;
-}
+import { useEffect, useState } from 'react';
+
+const width = 8;
+const candyColors = ['blue', 'green', 'red', 'purple', 'orange', 'yellow'];
+
+// function expression
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([]);
+
+  const createBoard = () => {
+    const randomColorArrangement = [];
+
+    for (let i = 0; i < width * width; i++) {
+      //random number from 0-5 (random color)
+      const randomColor =
+        candyColors[Math.floor(Math.random() * [candyColors.length])];
+      randomColorArrangement.push(randomColor);
+    }
+    setCurrentColorArrangement(randomColorArrangement);
+  };
+  useEffect(() => {
+    createBoard();
+  }, []);
+  console.log(currentColorArrangement);
+
+  return (
+    <div className='app'>
+      <div className='game'>
+        {currentColorArrangement.map((candyColor, idx) => (
+          <img key={idx} style={{ backgroundColor: candyColor }}></img>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default App;
